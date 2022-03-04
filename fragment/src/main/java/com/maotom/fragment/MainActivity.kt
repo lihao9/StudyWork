069c72191081b7ae40fc4eb814adf2f55f8e4a40
Alex <Alex@qq.com>
2022-03-04 18:40:54 +0800

    1

--- fragment/src/main/java/com/maotom/fragment/MainActivity.kt
+++ fragment/src/main/java/com/maotom/fragment/MainActivity.kt
@@ -4,8 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
     }
+
+    private fun initView() {
+        rv_main
+    }
+
+
 }
