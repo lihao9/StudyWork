5f35aaeee681bd58a5474804d76924d04b546796
Alex <Alex@qq.com>
2022-03-14 15:19:39 +0800

    databinding+viewbinding

--- view_data_binding/src/main/java/com/maotom/view_data_binding/bean/User.kt
+++ view_data_binding/src/main/java/com/maotom/view_data_binding/bean/User.kt
@@ -1,8 +1,9 @@
 package com.maotom.view_data_binding.bean
 
 /**
-*   @author:  Mao Tom
-*   @date:  2022/3/11 0011
-*   @description: todo
-*
-*/data class User()
+ *   @author:  Mao Tom
+ *   @date:  2022/3/11 0011
+ *   @description: todo
+ *
+ */
+data class User(var name: String,var age:Int)
